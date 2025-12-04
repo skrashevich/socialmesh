6bfa79c064deef6cbad4b8d69bb165339676dd47
Fulvio Cusumano <fcusumano@gmail.com>
2025-12-04 16:45:42 +1100

    feat(settings): add multiple new ringtone presets
    Add 15 new ringtone presets including popular themes, songs, and melodies:
    - 007 (James Bond theme)
    - 7Days melody
    - The Addams Family theme
    - Agadoo party song
    - Alice Cooper Poison riff
    - Alvin and the Chipmunks theme
    - Amazing Grace hymn
    - Axel F (Beverly Hills Cop)
    - Ba Ba Black Sheep nursery rhyme
    - Back to the Future theme
    - Barbie Girl by Aqua
    - Batman theme
    - Benny Hill chase theme
    - Beethoven melody
    - The Birdie Song
    
    This expands the built-in ringtone selection for users with more recognizable and fun options.

--- lib/features/settings/ringtone_screen.dart
+++ lib/features/settings/ringtone_screen.dart
@@ -72,6 +72,418 @@ const _builtInPresets = [
     rtttl: '24:d=16,o=6,b=200:e,p,e',
     description: 'Quick ping sound',
   ),
+  RingtonePreset(
+    name: '007',
+    rtttl:
+        '007:d=4,o=5,b=320:c,8d,8d,d,2d,c,c,c,c,8d#,8d#,2d#,d,d,d,c,8d,8d,d,2d,c,c,c,c,8d#,8d#,d#,2d#,d,c#,c,c6,1b.,g,f,1g.',
+    description: 'James Bond theme',
+  ),
+  RingtonePreset(
+    name: '7Days',
+    rtttl:
+        '7Days:d=16,o=6,b=140:g#,d#,8a#5,g#,d#,8a#5,g#,d#,8a#5,g#,d#,8a#5,f,1p5,g#,d#,8a#5,g#,d#,8a#5,g#,d#,8a#5,g#,d#,8a#5,f,1p5,g#,d#,8c,g#,d#,8c,g#,d#,8c,g#,d#,8c,f,1p5,g#,d#,8c,g#,d#,8c,g#,d#,8c,g#,d#,8c,f,1p5,d#,c,8g#5,d#,c,8g#5,d#,c,8g#5,d#,c,8g#5,f,1p5,d#,c,8g#5,d#,c,8g#5,d#,c,8g#5,d#,c,8g#5,f,1p5,d#,c#,8a#5,d#,c#,8a#5,d#,c#,8a#5,d#,c#,8a#5,f,1p5,d#,c#,8a#5,d#,c#,8a#5,d#,c#,8a#5,d#,c#,8a#5,f.',
+    description: '7 Days melody',
+  ),
+  RingtonePreset(
+    name: 'Adams Family',
+    rtttl:
+        'Adams Family:d=8,o=5,b=160:c,4f,a,4f,c,4b4,2g,f,4e,g,4e,g4,4c,2f,c,4f,a,4f,c,4b4,2g,f,4e,c,4d,e,1f,c,d,e,f,1p,d,e,f#,g,1p,d,e,f#,g,4p,d,e,f#,g,4p,c,d,e,f',
+    description: 'The Addams Family theme',
+  ),
+  RingtonePreset(
+    name: 'Agadoo',
+    rtttl:
+        'Agadoo:d=8,o=5,b=125:b,g#,4e,e,e,4e,e,e,e,e,d#,e,4f#,a,f#,4d#,d#,d#,4d#,d#,d#,d#,d#,c#,d#,4e',
+    description: 'Agadoo party song',
+  ),
+  RingtonePreset(
+    name: 'Alice Cooper - Poison',
+    rtttl:
+        'Alice Cooper-Poison:d=8,o=5,b=112:d,d,a,d,e6,d,d6,d,f#,g,c6,f#,g,c6,e,d,d,d,a,d,e6,d,d6,d,f#,g,c6,f#,g,c6,e,d,c,d,a,d,e6,d,d6,d,f#,g,c6,f#,g,c6,e,d,c,d,a,d,e6,d,d6,d,a,d,e6,d,d6',
+    description: 'Alice Cooper Poison riff',
+  ),
+  RingtonePreset(
+    name: 'Alvin and the Chipmunks',
+    rtttl:
+        'Alvin and the Chipmonks:d=4,o=5,b=285:g,p,c,c,a,c,c,c,p,b,b,c6,c6,p,c,c,p,a,g,d,2g,d,2p,a,g,d,2g,a,p,g,p,c,c,a,c,c,c,p,b,b,c6,c6,p,c,c,p,a,g,d,2g,d,2p,a,a,g,2a,g,p,2a,b,2a,g,p,c,c,c,c,c,e,e,e,e,2a,g,2a,2g,2a,a,2g,2g.,p,2a,g,2a,g,p,c,c,c,c,c,e,e,e,e,2a,g,2a,2g,2c6,b,2b,1b,2c6',
+    description: 'Alvin and the Chipmunks theme',
+  ),
+  RingtonePreset(
+    name: 'Amazing Grace',
+    rtttl:
+        'Amazing Grace:d=16,o=5,b=80:8c,2f,a,g,f,2a,8a,8g,2f,4d,2c,8c,2f,a,g,f,2a,8g,8a,2c6.',
+    description: 'Amazing Grace hymn',
+  ),
+  RingtonePreset(
+    name: 'Axel F',
+    rtttl:
+        'Axel:d=8,o=5,b=125:16g,16g,a#.,16g,16p,16g,c6,g,f,4g,d.6,16g,16p,16g,d#6,d6,a#,g,d6,g6,16g,16f,16p,16f,d,a#,2g,4p,16f6,d6,c6,a#,4g,a#.,16g,16p,16g,c6,g,f,4g,d.6,16g,16p,16g,d#6,d6,a#,g,d6,g6,16g,16f,16p,16f,d,a#,2g',
+    description: 'Axel F - Beverly Hills Cop',
+  ),
+  RingtonePreset(
+    name: 'Ba Ba Black Sheep',
+    rtttl:
+        'Ba Ba Black Sheep:d=8,o=5,b=150:c,4p,c,4p,g,4p,g,4p,4a,4b,4c6,4a,4g,4p,f,4p,f,4p,e,4p,e,4p,d,4p,d,4p,4c',
+    description: 'Ba Ba Black Sheep nursery rhyme',
+  ),
+  RingtonePreset(
+    name: 'Back to the Future',
+    rtttl:
+        'Back to the Future:d=16,o=5,b=200:4g.,p,4c.,p,2f#.,p,g.,p,a.,p,8g,p,8e,p,8c,p,4f#,p,g.,p,a.,p,8g.,p,8d.,p,8g.,p,8d.6,p,4d.6,p,4c#6,p,b.,p,c#.6,p,2d.6',
+    description: 'Back to the Future theme',
+  ),
+  RingtonePreset(
+    name: 'Barbie Girl',
+    rtttl:
+        'Barbie Girl:d=8,o=5,b=125:g#,e,g#,c#6,4a,4p,f#,d#,f#,b,4g#,f#,e,4p,e,c#,4f#,4c#,4p,f#,e,4g#,4f#',
+    description: 'Barbie Girl by Aqua',
+  ),
+  RingtonePreset(
+    name: 'Batman',
+    rtttl:
+        'Batman:d=8,o=5,b=180:d,d,c#,c#,c,c,c#,c#,d,d,c#,c#,c,c,c#,c#,d,d#,c,c#,c,c,c#,c#,f,p,4f',
+    description: 'Batman theme',
+  ),
+  RingtonePreset(
+    name: 'Benny Hill',
+    rtttl:
+        'Benny Hill:d=16,o=5,b=125:8d.,e,8g,8g,e,d,a4,b4,d,b4,8e,d,b4,a4,b4,8a4,a4,a#4,b4,d,e,d,4g,4p,d,e,d,8g,8g,e,d,a4,b4,d,b4,8e,d,b4,a4,b4,8d,d,d,f#,a,8f,4d,4p,d,e,d,8g,g,g,8g,g,g,8g,8g,e,8e.,8c,8c,8c,8c,e,g,a,g,a#,8g,a,b,a#,b,a,b,8d6,a,b,d6,8b,8g,8d,e6,b,b,d,8a,8g,4g',
+    description: 'Benny Hill chase theme',
+  ),
+  RingtonePreset(
+    name: 'Beethoven',
+    rtttl:
+        'Bethoven:d=4,o=5,b=160:c,e,c,g,c,c6,8b,8a,8g,8a,8g,8f,8e,8f,8e,8d,c,e,g,e,c6,g.',
+    description: 'Beethoven melody',
+  ),
+  RingtonePreset(
+    name: 'Birdy Song',
+    rtttl:
+        'Birdy Song:d=16,o=5,b=100:g,g,a,a,e,e,8g,g,g,a,a,e,e,8g,g,g,a,a,c6,c6,8b,8b,8a,8g,8f,f,f,g,g,d,d,8f,f,f,g,g,d,d,8f,f,f,g,g,a,b,8c6,8a,8g,8e,4c',
+    description: 'The Birdie Song',
+  ),
+  RingtonePreset(
+    name: 'Cantina',
+    rtttl:
+        'Cantina:d=8,o=5,b=250:a,p,d6,p,a,p,d6,p,a,d6,p,a,p,g#,4a,a,g#,a,4g,f#,g,f#,4f.,d.,16p,4p.,a,p,d6,p,a,p,d6,p,a,d6,p,a,p,g#,a,p,g,p,4g.,f#,g,p,c6,4a#,4a,4g',
+    description: 'Star Wars Cantina Band',
+  ),
+  RingtonePreset(
+    name: 'Chariots of Fire',
+    rtttl:
+        'Chariots of Fire:d=16,o=5,b=85:8c#,f#.,g#.,a#.,4g#,4f,8p,8c#,f#.,g#.,a#.,2g#,8p,8c#,f#.,g#.,a#.,4g#,4f,8p,8f,f#.,f.,c#.,2c#',
+    description: 'Chariots of Fire theme',
+  ),
+  RingtonePreset(
+    name: 'Coca Cola',
+    rtttl:
+        'Coca Cola:d=16,o=5,b=125:f#6,p,f#6,p,f#6,p,f#6,p,4g6,f#6,p,4e6,p,e6,p,8a6,4f#6,4d6',
+    description: 'Coca Cola jingle',
+  ),
+  RingtonePreset(
+    name: 'Dallas',
+    rtttl:
+        'Dallas:d=8,o=5,b=125:e,4a.,e,4e.6,a,4c#6,b,c#6,4a,4e,4a,4f#6,4e6,c#6,d6,2e.6,p,e,4a,4f#6,4e6,c#6,d6,4e6,b,c#6,4a,4e,4a,c#6,d6,4b.,a,2a',
+    description: 'Dallas TV theme',
+  ),
+  RingtonePreset(
+    name: 'Death March',
+    rtttl:
+        'Death March:d=4,o=5,b=100:4c,16p,c,8c,32p,2c,d#,8d,32p,d,8c,32p,c,8b4,32p,2c.',
+    description: 'Funeral march',
+  ),
+  RingtonePreset(
+    name: 'Deep Purple - Smoke on the Water',
+    rtttl:
+        'Deep Purple-Smoke on the Water:d=4,o=4,b=112:c,d#,f.,c,d#,8f#,f,p,c,d#,f.,d#,c,2p,8p,c,d#,f.,c,d#,8f#,f,p,c,d#,f.,d#,c',
+    description: 'Smoke on the Water riff',
+  ),
+  RingtonePreset(
+    name: 'Entertainer',
+    rtttl:
+        'Entertainer:d=8,o=5,b=140:d,d#,e,4c6,e,4c6,e,2c.6,c6,d6,d#6,e6,c6,d6,4e6,b,4d6,2c6,4p,d,d#,e,4c6,e,4c6,e,2c.6,p,a,g,f#,a,c6,4e6,d6,c6,a,2d6',
+    description: 'The Entertainer ragtime',
+  ),
+  RingtonePreset(
+    name: 'Final Countdown',
+    rtttl:
+        'Final Countdown:d=16,o=5,b=125:b,a,4b,4e,4p,8p,c6,b,8c6,8b,4a,4p,8p,c6,b,4c6,4e,4p,8p,a,g,8a,8g,8f#,8a,4g.,f#,g,4a.,g,a,8b,8a,8g,8f#,4e,4c6,2b.,b,c6,b,a,1b',
+    description: 'Final Countdown by Europe',
+  ),
+  RingtonePreset(
+    name: 'Flintstones',
+    rtttl:
+        'Flintstones:d=8,o=5,b=200:g#,4c#,p,4c#6,a#,4g#,4c#,p,4g#,f#,f,f,f#,g#,4c#,4d#,2f,2p,4g#,4c#,p,4c#6,a#,4g#,4c#,p,4g#,f#,f,f,f#,g#,4c#,4d#,2c#',
+    description: 'The Flintstones theme',
+  ),
+  RingtonePreset(
+    name: 'Friends',
+    rtttl:
+        'Friends:d=4,o=5,b=80:c,g,a#4,f,c,g,a#4,8a#,8e,c,g,a#4,f,c,g,a#4,8a#,8e',
+    description: 'Friends TV theme',
+  ),
+  RingtonePreset(
+    name: 'Funky Town',
+    rtttl:
+        'Funky Town:d=8,o=4,b=125:c6,c6,a#5,c6,p,g5,p,g5,c6,f6,e6,c6,2p,c6,c6,a#5,c6,p,g5,p,g5,c6,f6,e6,c6',
+    description: 'Funky Town disco',
+  ),
+  RingtonePreset(
+    name: 'Futurama',
+    rtttl:
+        'Futurama:d=8,o=5,b=112:e,4e,4e,a,4a,4d,4d,e,4e,4e,e,4a,4g#,4d,d,f#,f#,4e,4e,e,4a,4g#,4b,16b,16b,g,g,f#,f#,4e,4e,a,4a,4d,4d,e,g,f#,4e,e,4a,4g#,4d,d,f#,f#,4e,4e,e,4a,4g#,4b,16b,16b,g,g,f#,f#,p,16e,16e,e,d#,d,d,c#,c#',
+    description: 'Futurama theme',
+  ),
+  RingtonePreset(
+    name: 'Ghost Busters',
+    rtttl:
+        'Ghost Busters:d=8,o=5,b=145:16c6,32p,16c6,e6,c6,d6,a#,2p,32c6,32p,32c6,32p,c6,a#,c6',
+    description: 'Ghostbusters theme',
+  ),
+  RingtonePreset(
+    name: 'Greensleeves',
+    rtttl:
+        'Greensleaves:d=4,o=5,b=140:g,2a#,c6,d.6,8d#6,d6,2c6,a,f.,8g,a,2a#,g,g.,8f,g,2a,f,2d,g,2a#,c6,d.6,8e6,d6,2c6,a,f.,8g,a,a#.,8a,g,f#.,8e,f#,2g',
+    description: 'Greensleeves folk song',
+  ),
+  RingtonePreset(
+    name: 'Halloween',
+    rtttl:
+        'Halloween:d=8,o=5,b=180:d6,g,g,d6,g,g,d6,g,d#6,g,d6,g,g,d6,g,g,d6,g,d#6,g,c#6,f#,f#,c#6,f#,f#,c#6,f#,d6,f#,c#6,f#,f#,c#6,f#,f#,c#6,f#,d6,f#',
+    description: 'Halloween theme',
+  ),
+  RingtonePreset(
+    name: 'Hawaii 5-0',
+    rtttl:
+        'Hawaii 5 0:d=16,o=6,b=240:8g#5,p,8g#5,p,8b5,p,4d#,p,2c#.,p,2g#5.,p,8g#5,p,8g#5,p,8f#5,p,4b5,p,1g#5,4p.,8g#5,p,8g#5,p,8b5,p,4d#,p,2c#,8p,2g#.,8p,8f#,p,8f#,p,8d#,p,4b5,p,1g#.,4p,2b,p,8a,8g#,8f#,8e,8d#,8c#,8d#,8b5,2c#,4p,8c#,p,8b,8a,8g,8f#,8d#,8c#,8b5,8c#,4d#,8c#,p,4b5,p,4c#.,p,2g#,4p,8f#,p,8f#,p,8d#,p,4b5,p,1c#6',
+    description: 'Hawaii Five-0 theme',
+  ),
+  RingtonePreset(
+    name: 'He-Man',
+    rtttl:
+        'Heman:d=8,o=6,b=160:g,g,g,4g,4e,g,a,g,f,4g,4c,g,a,g,f,4g,4e,c,2d.,4p,4e.,4a5,4c,e,f#,e,d,4e,4a5,e,f#,e,d,4e,4a5,g5,2a5',
+    description: 'He-Man theme',
+  ),
+  RingtonePreset(
+    name: 'Imperial March',
+    rtttl:
+        'Star Wars:d=8,o=6,b=180:f5,f5,f5,2a#5.,2f.,d#,d,c,2a#.,4f.,d#,d,c,2a#.,4f.,d#,d,d#,2c,4p,f5,f5,f5,2a#5.,2f.,d#,d,c,2a#.,4f.,d#,d,c,2a#.,4f.,d#,d,d#,2c',
+    description: 'Star Wars Imperial March',
+  ),
+  RingtonePreset(
+    name: 'Jingle Bells',
+    rtttl:
+        'Jingle Bells:d=4,o=5,b=170:b,b,b,p,b,b,b,p,b,d6,g.,8a,2b.,8p,c6,c6,c6.,8c6,c6,b,b,8b,8b,b,a,a,b,2a,2d6',
+    description: 'Jingle Bells Christmas',
+  ),
+  RingtonePreset(
+    name: 'Knight Rider',
+    rtttl:
+        'Knight Rider:d=32,o=5,b=63:16e,f,e,8b,16e6,f6,e6,8b,16e,f,e,16b,16e6,4d6,8p,4p,16e,f,e,8b,16e6,f6,e6,8b,16e,f,e,16b,16e6,4f6',
+    description: 'Knight Rider theme',
+  ),
+  RingtonePreset(
+    name: 'Let It Be',
+    rtttl:
+        'Let it be:d=8,o=5,b=100:16e6,d6,4c6,16e6,g6,a6,g.6,16g6,g6,e6,16d6,c6,16a,g,4e.6,4p,e6,16e6,f.6,e6,e6,d6,16p,16e6,16d6,d6,2c.6.',
+    description: 'Let It Be by The Beatles',
+  ),
+  RingtonePreset(
+    name: 'Macarena',
+    rtttl:
+        'Macarena:d=8,o=5,b=180:f,f,f,4f,f,f,f,f,f,f,f,a,c,c,4f,f,f,4f,f,f,f,f,f,f,d,c,4p,4f,f,f,4f,f,f,f,f,f,f,f,a,4p,2c.6,4a,c6,a,f,4p,2p',
+    description: 'Macarena dance',
+  ),
+  RingtonePreset(
+    name: 'Mission Impossible',
+    rtttl:
+        'Mission Impossible:d=16,o=5,b=100:32d,32d#,32d,32d#,32d,32d#,32d,32d#,32d,32d,32d#,32e,32f,32f#,32g,g,8p,g,8p,a#,p,c6,p,g,8p,g,8p,f,p,f#,p,g,8p,g,8p,a#,p,c6,p,g,8p,g,8p,f,p,f#,p,a#,g,2d,32p,a#,g,2c#,32p,a#,g,2c,p,a#4,c',
+    description: 'Mission Impossible theme',
+  ),
+  RingtonePreset(
+    name: 'Monty Python',
+    rtttl:
+        'Monty Python:d=8,o=5,b=180:d#6,d6,4c6,b,4a#,a,4g#,g,f,g,g#,4g,f,2a#,p,a#,g,p,g,g,f#,g,d#6,p,a#,a#,p,g,g#,p,g#,g#,p,a#,2c6,p,g#,f,p,f,f,e,f,d6,p,c6,c6,p,g#,g,p,g,g,p,g#,2a#,p,a#,g,p,g,g,f#,g,g6,p,d#6,d#6,p,a#,a,p,f6,f6,p,f6,2f6,p,d#6,4d6,f6,f6,e6,f6,4c6,f6,f6,e6,f6,a#,p,a,a#,p,a,2a#',
+    description: 'Monty Python theme',
+  ),
+  RingtonePreset(
+    name: 'Muppets',
+    rtttl:
+        'Muppets:d=4,o=5,b=250:c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,8a,8p,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,8e,8p,8e,g,2p,c6,c6,a,b,8a,b,g,p,c6,c6,a,8b,a,g.,p,e,e,g,f,8e,f,8c6,8c,8d,e,8e,d,8d,c',
+    description: 'The Muppet Show theme',
+  ),
+  RingtonePreset(
+    name: 'Pager',
+    rtttl: 'Pager:d=8,o=5,b=160:d6,16p,2d6,16p,d6,16p,2d6,16p,d6,16p,2d6.',
+    description: 'Classic pager sound',
+  ),
+  RingtonePreset(
+    name: 'Phantom of the Opera',
+    rtttl:
+        'Phantom:d=8,o=5,b=120:4d6,d6,4a6,a6,4b6,g6,4a6,a6,4d6,d6,4a6,a6,a6,g6,f6,4e6,e6,4d6,d6,4a6,a6,4b6,g6,4a6,a6,4d6,d6,4a6,a6,f6,e6,c#6,4d6,d6.',
+    description: 'Phantom of the Opera',
+  ),
+  RingtonePreset(
+    name: 'Pink Panther',
+    rtttl:
+        'Piccolo:d=8,o=5,b=320:d6,4g6,4g,4g6,d6,e6,d6,b,4g,4d,g,a,b,c6,4d6,4g6,1d6,4d6,4g6,4g,4g6,d6,e6,b,4g,4d,f,g,a,b,4c6,4f6,1c6',
+    description: 'Pink Panther theme',
+  ),
+  RingtonePreset(
+    name: 'Popcorn',
+    rtttl:
+        'Popcorn:d=16,o=5,b=160:a,p,g,p,a,p,e,p,c,p,e,p,8a4,8p,a,p,g,p,a,p,e,p,c,p,e,p,8a4,8p,a,p,b,p,c6,p,b,p,c6,p,a,p,b,p,a,p,b,p,g,p,a,p,g,p,a,p,f,8a,8p,a,p,g,p,a,p,e,p,c,p,e,p,8a4,8p,a,p,g,p,a,p,e,p,c,p,e,p,8a4,8p,a,p,b,p,c6,p,b,p,c6,p,a,p,b,p,a,p,b,p,g,p,a,p,g,p,a,p,b,4c6',
+    description: 'Popcorn electronic',
+  ),
+  RingtonePreset(
+    name: 'Popeye',
+    rtttl:
+        'Popeye:d=8,o=6,b=160:a5,c,c,c,4a#5,a5,4c,2p,c,d,a#5,d,4f,d,2c,p,c,d,a#5,d,f,e,d,c,d,c,a5,f5,a5,c,d,c,4a#5,g5,2f5',
+    description: 'Popeye the Sailor Man',
+  ),
+  RingtonePreset(
+    name: 'Scatman',
+    rtttl:
+        'Scatman:d=16,o=5,b=200:8b,b,32p,8b,b,32p,8b,2d6,p,c#.6,p.,8d6,p,c#6,8b,p,8f#,2p.,c#6,8p,d.6,p.,c#6,b,8p,8f#,2p,32p,2d6,p,c#6,8p,d.6,p.,c#6,a.,p.,8e,2p.,c#6,8p,d.6,p.,c#6,b,8p,8b,b,32p,8b,b,32p,8b,2d6,p,c#.6,p.,8d6,p,c#6,8b,p,8f#,2p.,c#6,8p,d.6,p.,c#6,b,8p,8f#,2p,32p,2d6,p,c#6,8p,d.6,p.,c#6,a.,p.,8e,2p.,c#6,8p,d.6,p.,c#6,a,8p,8e,2p,32p,f#.6,p.,b.,p.',
+    description: 'Scatman John',
+  ),
+  RingtonePreset(
+    name: 'Scooby Doo',
+    rtttl:
+        'Scooby Doo:d=8,o=6,b=160:e,e,d,d,2c,d,4e,2a5,a5,4b5,4g5,4e,d,4c,d,2e,4p,e,e,d,d,2c,d,4f,2a5,a5,4b5,4g5,4e,d,2c',
+    description: 'Scooby Doo theme',
+  ),
+  RingtonePreset(
+    name: 'Sesame Street',
+    rtttl:
+        'Sesame Street:d=4,o=5,b=160:2c6,a,2f,8f,8g,a,p,2c6,a,1f,p,2c6,a,2f,8f,8g,a,2b,c6,2d6,p,8c6,8d6,d#6,d6,c6,a,g,8g,8a,a#,a,8g,8c,8c,1c',
+    description: 'Sesame Street theme',
+  ),
+  RingtonePreset(
+    name: 'Simpsons',
+    rtttl:
+        'Simpsons:d=8,o=5,b=160:c.6,4e6,4f#6,a6,4g.6,4e6,4c6,a,f#,f#,f#,2g,p,p,f#,f#,f#,g,4a#.,c6,c6,c6,4c6',
+    description: 'The Simpsons theme',
+  ),
+  RingtonePreset(
+    name: 'Smurfs',
+    rtttl:
+        'Smurfs:d=4,o=5,b=200:2c6,f.6,8c6,d6,a#,2g,c.6,8a,f,a,2g,p,16g,16a,16a#,16b,2c6,f.6,8c6,d6,a#,2g,c.6,8a,a#,e,2f,p,16g,16a,16a#,16b,2c6,f.6,8c6,d6,a#,2g,c.6,8a,f,a,2g,p,16g,16a,16a#,16b,2c6,f.6,8c6,d6,a#,2g,c.6,8a,a#,e,2f.,1p',
+    description: 'The Smurfs theme',
+  ),
+  RingtonePreset(
+    name: 'South Park',
+    rtttl:
+        'South Park:d=8,o=5,b=125:e,e,e,16e,e,16e,p,e,e,e,e,e,16p,16e,e,2p,g,g,4g,16b,16b,b,e,a#,16g,16g,g,16f#,16f#,f#,e',
+    description: 'South Park theme',
+  ),
+  RingtonePreset(
+    name: 'Spiderman',
+    rtttl:
+        'Spiderman:d=4,o=6,b=200:c,8d#,g.,p,f#,8d#,c.,p,c,8d#,g,8g#,g,f#,8d#,c.,p,f,8g#,c.7,p,a#,8g#,f.,p,c,8d#,g.,p,f#,8d#,c,p,8g#,2g,p,8f#,f#,8d#,f,8d#,2c',
+    description: 'Spiderman theme',
+  ),
+  RingtonePreset(
+    name: 'Star Trek',
+    rtttl: 'Star Trek:d=16,o=5,b=63:8f.,a#,4d#.6,8d6,a#.,g.,c.6,4f6',
+    description: 'Star Trek theme',
+  ),
+  RingtonePreset(
+    name: 'Superman',
+    rtttl:
+        'Super Man:d=8,o=6,b=180:g5,g5,g5,4c,c,2g,p,g,a.,16g,f,1g,p,g5,g5,g5,4c,c,2g,p,g,a.,16g,f,a,2g.,4p,c,c,c,2b.,4g.,c,c,c,2b.,4g.,c,c,c,b,a,b,2c7,c,c,c,c,c,2c.',
+    description: 'Superman theme',
+  ),
+  RingtonePreset(
+    name: 'Take On Me',
+    rtttl:
+        'Take On Me:d=8,o=5,b=160:f#,f#,f#,d,p,b4,p,e,p,e,p,e,g#,g#,a,b,a,a,a,e,p,d,p,f#,p,f#,p,f#,e,e,f#,e,f#,f#,f#,d,p,b4,p,e,p,e,p,e,g#,g#,a,b,a,a,a,e,p,d,p,f#,p,f#,p,f#,e,e5',
+    description: 'Take On Me by A-ha',
+  ),
+  RingtonePreset(
+    name: 'Tarzan',
+    rtttl:
+        'Tarzan:d=8,o=5,b=120:e,f,g,16c6,g,16c6,16g,16c6,g,4d6,g,g.,4c6,4p,f,g,a,16c6,a,16c6,16a,16c6,a,4d6,g,g.,4c6,4p,e,f,g,16c6,g,16c6,16g,16c6,g,4d6,g,g.,4c6,4p,f,g,a,16c6,a,16c6,16a,16c6,a,4d6,g,g.,4c6.',
+    description: 'Tarzan Boy',
+  ),
+  RingtonePreset(
+    name: 'Teletubbies',
+    rtttl:
+        'Tele Tubbies:d=4,o=5,b=125:16g.,p,16g,e,4g.,p,4a,2f.,16g.,p,16g,e,4g.,p,2d.,16g.,p,16g,e,4g.,p,4a,2f.,2g,2b,2c.6.',
+    description: 'Teletubbies theme',
+  ),
+  RingtonePreset(
+    name: 'Teenage Mutant Ninja Turtles',
+    rtttl:
+        'Teenage Turtles:d=8,o=6,b=100:g5,a5,g5,a5,g5,16a5,g.5,a5,a#5,c,a#5,c,a#5,16c,a#.5,c,d#,f,d#,f,d#,16f,d#.,f,16c,16c,16c,16c,a#5,4c,16c7,16c7,16c7',
+    description: 'TMNT theme',
+  ),
+  RingtonePreset(
+    name: 'The A Team',
+    rtttl:
+        'The A Team:d=8,o=5,b=132:4d#6,a#,2d#6,16p,g#,4a#,4d#.,p,16g,16a#,d#6,a#,f6,2d#6,16p,c#.6,16c6,16a#,g#.,2a#.',
+    description: 'The A-Team theme',
+  ),
+  RingtonePreset(
+    name: 'Thunderbirds',
+    rtttl:
+        'Thunder Birds:d=8,o=4,b=125:g#5,16f5,16g#5,4a#5,p,16d#5,16f5,g#5,a#5,d#6,16f6,16c6,d#6,f6,2a#5,g#5,16f5,16g#5,4a#5,p,16d#5,16f5,g#5,a#5,d#6,16f6,16c6,d#6,f6,2g6,g6,16a6,16e6,4g6,p,16e6,16d6,c6,b5,a.5,16b5,c6,e6,2d6,d#6,16f6,16c6,4d#6,p,16c6,16a#5,g#5,g5,f.5,16g5,g#5,a#5,c6,a#5,g5,d#5',
+    description: 'Thunderbirds theme',
+  ),
+  RingtonePreset(
+    name: 'Titanic',
+    rtttl:
+        'Titanic:d=8,o=6,b=120:c,d,2e.,d,c,d,g,2g,f,e,4c,2a5,g5,f5,16d5,16e5,2d5,p,c,d,2e.,d,c,d,g,2g,e,g,2a,2g,16d,16e,2d.',
+    description: 'My Heart Will Go On',
+  ),
+  RingtonePreset(
+    name: 'Toccata',
+    rtttl:
+        'Toccata:d=16,o=5,b=160:a,g,1a,g,f,e,d,2c#,p,2d,2p,a,g,1a,8e.,4f,2c#,2d',
+    description: 'Toccata and Fugue',
+  ),
+  RingtonePreset(
+    name: 'Transformers',
+    rtttl:
+        'Transformers:d=16,o=6,b=285:e7,f7,e7,d#7,4d7,4p,d,d,d,d,d,d,d,d,e,e,e,e,f,f,f,f,f,f,f,f,8a7,8a#7,8a7,8p,4d7,2p,d,d7,d,d7,d,d7,d,d7,e,e7,e,e7,f,f7,f,f7,f,f7,f,f7,a5,a5,a5,a5,a#5,a#,a#5,a#,a#5,a#,a#5,a#,a#5,a#,a#5,a#,4p,8d,8p,4e,4f,4p,4f,4p,2g,4a,4a#,4p,g,g7,g,g7,g,g7,g,g7,4e,4g,4a,4p,f,f7,f,f7,f,f7,f,f7,4e,4f,4g,4p,e,e7,e,e7,e,e7,e,e7,e,e7,e,e7,4p,4d,4c#,8e,8p,4d,2d,d,d7,d,d7,d,d7,d,d7',
+    description: 'Transformers theme',
+  ),
+  RingtonePreset(
+    name: 'Tubular Bells',
+    rtttl:
+        'Tubular Bells:d=4,o=5,b=280:c6,f6,c6,g6,c6,d#6,f6,c6,g#6,c6,a#6,c6,g6,g#6,c6,g6,c6,f6,c6,g6,c6,d#6,f6,c6,g#6,c6,a#6,c6,g6,g#6,c6,g6,c6,f6,c6,g6,c6,d#6,f6,c6,g#6,c6,a#6,c6,g6,g#6,c6,g6,c6,f6,c6,g6,c6,d#6,f6,c6,g#6,c6,a#6,c6,g6,g#6',
+    description: 'Tubular Bells - Exorcist',
+  ),
+  RingtonePreset(
+    name: 'Under the Sea',
+    rtttl:
+        'Under the Sea:d=4,o=6,b=200:8d,8f,8a#,d7,d7,8a#,c7,d#7,d7,a#,8a#5,8d,8f,a#,a#,8c,a,c7,a#,p,8d,8f,8a#,d7,d7,8a#,c7,d#7,d7,a#,8a#5,8d,8f,a#,a#,8c,a,c7,16a#,16d,16a#,16d,16a#,16d,16a#',
+    description: 'Under the Sea - Little Mermaid',
+  ),
+  RingtonePreset(
+    name: 'USA National Anthem',
+    rtttl:
+        'USA National Anthem:d=8,o=5,b=120:e.,d,4c,4e,4g,4c6.,p,e6.,d6,4c6,4e,4f#,4g.,p,4g,4e6.,d6,4c6,2b,a,4b,c6.,16p,4c6,4g,4e,32p,4c',
+    description: 'Star-Spangled Banner',
+  ),
+  RingtonePreset(
+    name: 'Wannabe',
+    rtttl:
+        'Wannabe:d=8,o=5,b=125:16g,16g,16g,16g,g,a,g,e,p,16c,16d,16c,d,d,c,4e,4p,g,g,g,a,g,e,p,4c6,c6,b,g,a,16b,16a,4g',
+    description: 'Wannabe by Spice Girls',
+  ),
+  RingtonePreset(
+    name: 'We Wish You a Merry Christmas',
+    rtttl:
+        'We Wish you a Merry Christmas:d=8,o=5,b=140:4d,4g,g,a,g,f#,4e,4c,4e,4a,a,b,a,g,4f#,4d,4f#,4b,b,c6,b,a,4g,4e,4d,4e,4a,4f#,2g',
+    description: 'Merry Christmas carol',
+  ),
+  RingtonePreset(
+    name: 'YMCA',
+    rtttl:
+        'YMCA:d=8,o=5,b=160:c#6,a#,2p,a#,g#,f#,g#,a#,4c#6,a#,4c#6,d#6,a#,2p,a#,g#,f#,g#,a#,4c#6,a#,4c#6,d#6,b,2p,b,a#,g#,a#,b,4d#6,f#6,4d#6,4f.6,4d#.6,4c#.6,4b.,4a#,4g#',
+    description: 'YMCA by Village People',
+  ),
 ];
 
 /// Provider for custom ringtone presets stored in SharedPreferences
